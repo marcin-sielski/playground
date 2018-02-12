1d70f70d19f0cd10900a403625e2989690b2bb88
Marcin Sielski <marcin.sielski@gmail.com>
2018-02-12 09:04:35 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,31 @@
+self.addEventHandler('statechange', function(event) {
 
+});
+
+self.addEventHandler('updatefound', function(event) {
+
+});
+
+self.addEventHandler('controllerchange', function(event) {
+
+});
+
+self.addEventHandler('error', function(event) {
+
+});
+
+self.addEventHandler('message', function(event) {
+
+});
+
+self.addEventHandler('install', function(event) {
+
+});
+
+self.addEventHandler('activate', function(event) {
+
+});
+
+self.addEventHandler('fetch', function(event) {
+
+});
