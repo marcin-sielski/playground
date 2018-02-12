b9476f8d4ed74f4f49a3a7e0df00a41362db11a4
Marcin Sielski <marcin.sielski@gmail.com>
2018-02-12 10:06:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -23,7 +23,22 @@ self.addEventHandler('install', function(event) {
 });
 
 self.addEventHandler('activate', function(event) {
+  var title = 'Yay a message.';
+  var body = 'We have received a push message.';
+  var tag = 'simple-push-demo-notification-tag';
+  var data = {
+    doge: {
+        wow: 'such amaze notification data'
+    }
+  };
 
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      tag: tag,
+      data: data
+    })
+  );
 });
 
 self.addEventHandler('fetch', function(event) {
